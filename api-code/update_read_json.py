f9ee677bdd434c4b506e8a834776ac0b7ecefa2e
riasto1 <nhanitiu@gmail.com>
2019-06-22 17:23:59 +0700

    Update function for update, read json data file

--- api-code/update_read_json.py
+++ api-code/update_read_json.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 
 def load_data(path):
@@ -15,8 +16,12 @@ def load_data(path):
             new_dict[i['name']]['calories'] = i['calories']
             new_dict[i['name']]['proteins'] = i['proteins']
             new_dict[i['name']]['carbohydrates'] = i['carbohydrates']
-            new_dict[i['name']]['serving'] = i['serving']
+            new_dict[i['name']]['serving'] = 1
             new_dict[i['name']]['nutrients'] = i['nutrients']
+            if 'fibers' not in i:
+                new_dict[i['name']]['fibers'] = 0
+            else:
+                new_dict[i['name']]['fibers'] = i['fibers']
         else:
             pass
     return new_dict
@@ -35,8 +40,20 @@ def access_new_json(path):
 
 
 if __name__ == "__main__":
-    if not os.path.exists(os.path.join(os.getcwd(), "new_foods.json")):
+    if len(sys.argv) != 2:
+        print("Usage: update_read_json.py --update or update_read_json.py --access")
+    elif sys.argv[1] == '--update':
+        '''
+        The old food json file is readable but not in correct format we want to use
+        Write to new json file. However not really readable
+        Manually add or update records in old json is preferable
+        '''
+        print("Update new_foods.json for processing\n")
         new_dict = load_data(os.path.join(os.getcwd(), "foods.json"))
         write_new_json(os.path.join(os.getcwd(), "new_foods.json"), new_dict)
-    else:
+    elif sys.argv[1] == '--access':
+        '''This will be modify later to get data for processing if required'''
+        print("Accessing new json files\n")
         access_new_json(os.path.join(os.getcwd(), "new_foods.json"))
+    else:
+        print("The arguments only accept: --access or --update")
